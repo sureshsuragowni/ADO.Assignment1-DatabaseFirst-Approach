5a3336259bb55fa3b5ac7251b6ac17e798545fff
Suragowni <suresh-gowd.suragowni@capgemini.com>
2022-05-05 12:38:32 +0530

    commited

---
+++ Models/FootBall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FootBall2.Models
+{
+    public class FootBall
+    {
+
+        public int MatchID { get; set; }
+        public string TeamName1 { get; set; }
+        public string TeamName2 { get; set; }
+        public string Status { get; set; }
+        public string WinningTeam { get; set; }
+        public Nullable<int> Points { get; set; }
+    }
+}
